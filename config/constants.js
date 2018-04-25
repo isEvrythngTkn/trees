8634a2b0fa884e37119a054a98e9704dfa9967fc
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-04-25 09:20:34 -0700

    Adds some boilerplate for redux

---
+++ config/constants.js
@@ -0,0 +1,6 @@
+export const FIREBASE_API_KEY = 'AIzaSyCJw9C4MaKcLRoernDi6Pk-G6QDhrj2AM8';
+export const FIREBASE_AUTH_DOMAIN = 'trees-eefc8.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://trees-eefc8.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'trees-eefc8';
+export const FIREBASE_STORAGE_BUCKET = 'trees-eefc8.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '1092138657950';
---
+++ redux/actions/AuthActions.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER
+} from './types';
+
+export const emailChanged = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    };
+};
+
+export const loginUser = ({ email, password }) => {};
+
+const loginUserFail = (dispatch) => {};
+
+const loginUserSuccess = (dispatch, user) => {};
