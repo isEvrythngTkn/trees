0e0625c90cefa149a13f44e45e96198a3efc0c30
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-05-18 10:38:56 -0700

    Makes components out of the header elememts and passes them through the navigation options

--- components/NewHeader.js
+++ components/NewHeader.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React from 'react';
 import {
   StyleSheet,
@@ -6,57 +5,29 @@ import {
   View,
   Image,
   TouchableOpacity,
-  ImageBackground
 } from 'react-native';
-import { Button } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
-import { MyAppTitleText, MyAppText, MontserratBlack } from './StyledText';
+import { MyAppTitleText, MyAppText } from './StyledText';
 import { connect } from 'react-redux';
-import { playAgain } from '../redux/actions/PlayActions';
 
-class NewHeader extends React.Component {
-  onSettingsPress() {
-    this.props.navigation.navigate('Settings');
-    this.props.playAgain();
-  }
-
-  renderHamburger() {
-    if (_.indexOf(['OrderDetails', 'Orders'], this.props.navigation.state.routeName) < 0) {
-      return (
-        <View style={styles.hamburger}>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate("DrawerOpen")}>
-            <Ionicons
-              name='md-menu'
-              size={36}
-              color={this.props.tintColor}
-            />
-          </TouchableOpacity>
-        </View>
-      );
-    } else {
-      <View style={styles.hamburger} />
-    }
-  }
-
-  renderBalance() {
-    const iconSource = this.props.tintColor === 'black' ? 
-      require('../assets/images/tree-with-many-leaves_black.png') : require('../assets/images/icon-tree3.png');
-
+export class Hamburger extends React.Component {
+  render() {
     return (
-      <View style={styles.balanceTextContainerStyle}>
-        <MyAppText>
-          <Text style={[styles.balanceTextStyle, { color: this.props.tintColor }]}>{this.props.balance}</Text>
-        </MyAppText>
-        <Image
-          source={iconSource}
-          style={styles.icon}
-          resizeMode="contain"
-        />
+      <View style={styles.hamburger}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+          <Ionicons
+            name='md-menu'
+            size={36}
+            color={this.props.tintColor}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
+}
 
-  renderStandardTitle() {
+export class Title extends React.Component {
+  render() {
     return (
       <View style={styles.titleTextContainerStyle}>
         <Text style={[styles.titleTextStyle, { color: this.props.tintColor}]}>
@@ -67,44 +38,48 @@ class NewHeader extends React.Component {
       </View>
     );
   }
-
-  render() {
-    return (
-      <View style={styles.headerContainerStyle}>
-        {this.renderHamburger()}
-        {this.renderStandardTitle()}
-        {this.renderBalance()}
-      </View>
-    );
-  }
 }
 
+let Balance = ({ tintColor, balance }) => {
+  const iconSource = tintColor === 'black' ? 
+    require('../assets/images/tree-with-many-leaves_black.png') : require('../assets/images/icon-tree3.png');
 
+  return (
+    <View style={styles.balanceTextContainerStyle}>
+      <MyAppText>
+        <Text style={[styles.balanceTextStyle, { color: tintColor }]}>{balance}</Text>
+      </MyAppText>
+      <Image
+        source={iconSource}
+        style={styles.icon}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const mapStateToProps = state => {
+  const { balance } = state.ost;
+  return { balance };
+}
+
+export const HeaderBalance = connect(mapStateToProps, {})(Balance);
 
 const styles = StyleSheet.create({
-  headerContainerStyle: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
   hamburger: {
-    flex: 1
+    paddingLeft: 15,
   },
   titleTextContainerStyle: {
-    flex: 3,
+    flex: 1,
     alignItems: 'center',
   },
   titleTextStyle: {
     fontSize: 40,
-    lineHeight: 62,
-    marginTop: 8, 
+    marginTop: 5, 
     color: 'white'
   },
   balanceTextContainerStyle: {
-    flex: 1,
+    paddingRight: 15,
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
@@ -116,17 +91,5 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
     marginLeft: 5
-  },
-  settings: {
-    marginRight: 25
   }
 });
-
-const mapStateToProps = state => {
-  const { balance } = state.ost;
-  return {
-    balance
-  };
-}
-
-export default connect(mapStateToProps, { playAgain })(NewHeader);
--- navigation/NavigationOptions.js
+++ navigation/NavigationOptions.js
@@ -1,32 +1,30 @@
+import _ from 'lodash';
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import NewHeader from '../components/NewHeader';
-import HomeHeader from '../components/HomeHeader';
+import { HeaderBackButton } from 'react-navigation';
+import { Hamburger, HeaderBalance, Title } from '../components/NewHeader';
 import store from '../redux/store';
 import { PLAY_AGAIN } from '../redux/actions/types';
 
-const _sharedNavigationOptions = (navigation, headerType, height, backgroundColor, tintColor, wordmark) => {
+const _sharedNavigationOptions = (navigation, headerType, height, backgroundColor, tintColor) => {
   const { params } = navigation.state;
-  const balance = params ? params.balance : 'fetching';
+  const balance = params ? params.balance : '...';
   const title = params ? params.title : '';
 
-  let headerTitle;
-
-  headerTitle = ( <NewHeader 
-                    balance={balance} 
-                    title={title} 
-                    navigation={navigation} 
-                    tintColor={tintColor} 
-                    wordmark={wordmark}/> );
+  const headerLeft = _.indexOf(['OrderDetails', 'Orders'], navigation.state.routeName) < 0 ?
+    <Hamburger navigation={navigation} tintColor={tintColor} /> : 
+    <HeaderBackButton onPress={() => navigation.goBack(null)} tintColor={tintColor} />;
 
   return {
-    headerTitle: headerTitle,
+    headerTitle: (<Title tintColor={tintColor} title={title} />),
+    headerRight: (<HeaderBalance tintColor={tintColor} balance={balance} />),
+    headerLeft: headerLeft,
     headerTintColor: tintColor,
     headerStyle: { 
       height: height,
       elevation: 0,       //remove shadow on Android
       shadowOpacity: 0,   //remove shadow on iOS,
-      backgroundColor: backgroundColor
+      backgroundColor: backgroundColor,
+      borderBottomWidth: 0,
     },
     tabBarOnPress: ({ previousScene, scene, jumpToIndex }) => {
       store.dispatch({ type: PLAY_AGAIN });
@@ -36,10 +34,10 @@ const _sharedNavigationOptions = (navigation, headerType, height, backgroundColo
 };
 
 export const getNavigationOptions = ({ navigation }) => {
-  return _sharedNavigationOptions(navigation, 'standard', 72, '#489174', 'white', false);
+  return _sharedNavigationOptions(navigation, 'standard', 72, '#489174', 'white');
 };
 
 export const getHomeNavigationOptions = ({ navigation }) => {
-  return _sharedNavigationOptions(navigation, 'standard', 72, '#cef5e3', 'black', true);
+  return _sharedNavigationOptions(navigation, 'standard', 72, '#cef5e3', 'black');
 };
 
